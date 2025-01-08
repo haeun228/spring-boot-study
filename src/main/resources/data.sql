4f7fd5e1b18ffd89e147ba8177d7175ccced19fc
haeun228 <123haeun@naver.com>
2025-01-08 11:21:32 +0900

    12. 서비스 계층과 트랜잭션

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO article(title, content) VALUES('가가가가', '1111');
 INSERT INTO article(title, content) VALUES('나나나나', '2222');
-INSERT INTO article(title, content) VALUES('다다다다', '2222');
+INSERT INTO article(title, content) VALUES('다다다다', '3333');
 
 -- 11장 셀프체크 데이터 추가
 INSERT INTO coffee(name, price) VALUES('아메리카노', '4500');
